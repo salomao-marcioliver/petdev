ef4f8b62e2e5c5aaeb34876dc8488ebe155de06d
Salomão Marcioliver <salomaomarcioliver@gmail.com>
2022-08-19 10:39:22 -0300

    final project

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,48 @@
 import { Request, Response } from "express";
+import { Pet } from "../models/pet";
+import { createMenuObject } from '../helpers/createMenuObject';
 
 export const home = (req: Request, res: Response) => {
-    
+    let list = Pet.getAll();
+    res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        },
+        list
+    })
 }
 export const dogs = (req: Request, res: Response) => {
-    
+    let list = Pet.getFromType('dog')
+    res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        },
+        list
+    })
 }
 export const cats = (req: Request, res: Response) => {
-    
+    let list = Pet.getFromType('cat')
+    res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        },
+        list
+    })
 }
 export const fiches = (req: Request, res: Response) => {
-    
+    let list = Pet.getFromType('fish')
+    res.render('pages/page', {
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Peixe',
+            background: 'banner_fish.jpg'
+        },
+        list
+    })
 }
